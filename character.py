bc5ad5f8011da4ef21e24d8cc9b0cfdacec94eaa
brayan-nandan <brayan.nandan@edu.esiee.fr>
2026-01-05 10:30:58 -0400

    Create Character class with initialization method
    Added a Character class with initialization and string representation.

--- character.py
+++ character.py
@@ -1,1 +1,11 @@
+# Define the Character class.
+class Character():
+  
+    def __init__(self, name, description, current_room, msgs):
+        self.name = name
+        self.description = description
+        self.current_room = current_room
+        self.msgs = msgs
 
+    def __str__(self):
+        return f"{self.name} : {self.description}"
